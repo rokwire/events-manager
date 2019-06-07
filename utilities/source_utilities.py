e7f5ba6f3c79157ba40788ba469dd65dedfb3665
ben~know nothing <span14@illinois.edu>
2019-06-07 15:49:03 -0500

    Task38 srcdb (#43)
    * update source event utilities, source event parsing, and constant mapping
    
    * update db exception printout
    
    * remove default status

--- utilities/source_utilities.py
+++ utilities/source_utilities.py
@@ -1,3 +1,11 @@
-# get all events in a calendar
-def get_calendar_events(calendarId):
-    pass
+from flask import current_app
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
+
+from ..db import find_all, find_one, update_one 
+
+
+def get_calendar_events(sourceId, calendarId):
+    return find_all(current_app.config['EVENT_COLLECTION'], filter={"sourceId": sourceId, 
+                                                                    "calendarId": calendarId})
+
