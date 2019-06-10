7b2645017fbd7e66cb46e3d0c559bf329701f829
hjiang2 <32148302+hjiang2@users.noreply.github.com>
2019-06-10 15:58:54 -0500

    Task33 edit update (#44)
    * events edit page.

--- user_events.py
+++ user_events.py
@@ -1,5 +1,6 @@
-from flask import Flask,render_template,url_for,flash, redirect, Blueprint
+from flask import Flask,render_template,url_for,flash, redirect, Blueprint, request
 from .utilities.user_utilities import *
+from .utilities.constants import *
 
 userbp = Blueprint('user_events', __name__, url_prefix='/user-events')
 
@@ -10,8 +11,26 @@ def user_events():
 
 @userbp.route('/event<id>',  methods=['GET'])
 def user_an_event(id):
-	return render_template("events/event.html", post = find_user_event(id))
+    post = find_user_event(id)
+    return render_template("events/event.html", post = post)
 
 @userbp.route('/event<id>/edit', methods=['GET', 'POST'])
 def user_an_event_edit(id):
-    return render_template("events/event-edit.html", post = find_user_event(id))
+    post_by_id = find_user_event(id)
+    if request.method == 'POST':
+        # change the specific event
+        post_by_id['titleURL'] = request.form['titleURL']
+        post_by_id['startDate'] = request.form['startDate']
+        post_by_id['endDate'] = request.form['endDate']
+        post_by_id['cost'] = request.form['cost']
+        post_by_id['sponsor'] = request.form['sponsor']
+        # more parts editable TODO ....
+
+        # insert update_user_event function here later
+        update_user_event(id, post_by_id)
+    return render_template("events/event-edit.html", post = post_by_id, eventTypeMap = eventTypeMap)
+
+@userbp.route('/event<id>/approve')
+def user_an_event_approve(id):
+    update_user_event(id, {"eventStatus": "approve"})
+    return redirect(url_for("user_events.user_an_event", id=id))
