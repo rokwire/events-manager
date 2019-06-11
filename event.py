f64b6daf005b41dfb47a8f90af189a750e05bc6f
Panqiu Tang <panqiut2@illinois.edu>
2019-06-11 14:44:21 -0500

    download calendars ui

--- event.py
+++ event.py
@@ -6,28 +6,29 @@ from werkzeug.exceptions import abort
 from .auth import login_required
 from .db import find_all
 
-from .utilities.constants import int2SrcDB
 from .utilities.source_utilities import *
+from .utilities.sourceEvents import start
+
+import sys
+
 
 bp = Blueprint('event', __name__, url_prefix='/event')
 
 @bp.route('/source/<sourceId>')
-@login_required
 def source(sourceId):
     # page = request.args.get('page', 0, type=int)
-    allsources = int2SrcDB
+    allsources = current_app.config['INT2SRC']
     title = allsources[sourceId][0]
     calendars = allsources[sourceId][1]
     return render_template('events/source-events.html', allsources=allsources, sourceId=sourceId, title=title, calendars=calendars, total=0)
 
 @bp.route('/calendar/<calendarId>')
-@login_required
 def calendar(calendarId):
     # find source of current calendar
     sourceId = 0
     sourcetitle = "error: None"
     title = ""
-    for key, source in int2SrcDB.items():
+    for key, source in current_app.config['INT2SRC'].items():
         for item in source[1]:
             if calendarId in item:
                 title = item[calendarId]
@@ -40,4 +41,11 @@ def calendar(calendarId):
 @bp.route('/setting')
 @login_required
 def setting():
-    return render_template('events/setting.html', sources=int2SrcDB)
+    return render_template('events/setting.html', sources=current_app.config['INT2SRC'])
+
+@bp.route('/download')
+@login_required
+def download():
+    print("downloaded", file=sys.stderr)
+    start()
+    return redirect(url_for('event.setting'))
