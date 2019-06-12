30ba25df58f5d420ac2233f9f9c2272224264c13
hjiang2 <32148302+hjiang2@users.noreply.github.com>
2019-06-12 13:32:47 -0500

    Task49 simple search (#53)
    * display approved/disapproved/published--not in real format need edit later
    
    * add a function to find all searched events
    
    * Edit user page filter layout add and edit css style
    
    * add simple search functionality in user-event page
    
    * improve input validation

--- user_events.py
+++ user_events.py
@@ -4,9 +4,33 @@ from .utilities.constants import *
 
 userbp = Blueprint('user_events', __name__, url_prefix='/user-events')
 
-@userbp.route('/')
+@userbp.route('/', methods=['GET', 'POST'])
 def user_events():
-    posts = get_all_user_events()
+    if request.method == 'POST':
+		#format : 'id=1234' /'type=Academic'/'id=1234&type=Academic'
+        searchInput = request.form['searchInput']
+        search_id = None
+        search_type = None
+        if '&' in searchInput:
+        	input_list = searchInput.split('&')
+        	if len(input_list[0]) > 3 and input_list[0][0:2]=='id':
+        		search_id = input_list[0][3:]
+        	if len(input_list[1]) > 5 and input_list[1][0:4]=='type':
+        		search_type = input_list[1][5:]
+        else:
+        	if len(searchInput) > 3 and searchInput[0:2]=='id':
+        		search_id = searchInput[3:]
+        	elif len(searchInput) > 5 and searchInput[0:4]=='type':
+        		search_type = searchInput[5:]
+        result_dic = {}
+        if search_id != None:
+        	result_dic['eventId'] = search_id
+        if search_type != None:
+        	result_dic['eventType'] = search_type
+        #print(result_dic)
+        posts = get_searched_user_events(result_dic)
+    else:
+        posts = get_all_user_events()
     return render_template("events/user-events.html", posts=posts)
 
 @userbp.route('/event<id>',  methods=['GET'])
