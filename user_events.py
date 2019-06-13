731eac26374c2cb32b826935756cbd5846c80375
hjiang2 <32148302+hjiang2@users.noreply.github.com>
2019-06-13 13:34:01 -0500

    Task55 category redefine (#57)
    * add new row-content style
    
    * edit user-events endpoints
    
    * set format for backtoall button
    
    * add location address
    
    * add location address
    
    * add subcategory feature
    
    * add subcategory feature
    
    * redefine category
    
    * redefine category in edit and create a dic for new categorys
    
    * redefine category in edit page
    
    * delete row-content css
    
    * delete row-content in htmls
    
    * add row content in filter
    
    * adjust format of description display

--- user_events.py
+++ user_events.py
@@ -39,25 +39,31 @@ def user_events():
 @userbp.route('/event/<id>',  methods=['GET'])
 def user_an_event(id):
     post = find_user_event(id)
-    return render_template("events/event.html", post = post, isUser=True)
+    return render_template("events/event.html", post = post, eventTypeMap = eventTypeMap, isUser=True)
 
-@userbp.route('/event<id>/edit', methods=['GET', 'POST'])
+@userbp.route('/event/<id>/edit', methods=['GET', 'POST'])
 def user_an_event_edit(id):
     post_by_id = find_user_event(id)
     if request.method == 'POST':
         # change the specific event
         post_by_id['titleURL'] = request.form['titleURL']
+        post_by_id['subcategory'] = request.form['subcategory']
         post_by_id['startDate'] = request.form['startDate']
         post_by_id['endDate'] = request.form['endDate']
         post_by_id['cost'] = request.form['cost']
         post_by_id['sponsor'] = request.form['sponsor']
         # more parts editable TODO ....
 
-        # insert update_user_event function here later
         update_user_event(id, post_by_id)
-    return render_template("events/event-edit.html", post = post_by_id, eventTypeMap = eventTypeMap, isUser=True)
 
-@userbp.route('/event<id>/approve')
+        # create dic for eventType values - new category
+        eventTypeValues = {}
+        for key in eventTypeMap:
+            value = eventTypeMap[key]
+            eventTypeValues[value] = 0
+    return render_template("events/event-edit.html", post = post_by_id, eventTypeMap = eventTypeMap, eventTypeValues = eventTypeValues, isUser=True)
+
+@userbp.route('/event/<id>/approve')
 def user_an_event_approve(id):
     try:
         update_user_event(id, {"eventStatus": "approved"})
