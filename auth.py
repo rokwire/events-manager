6d388d81d738aeb73a469751a977fa65aa6579d3
span14 <span14@illinois.edu>
2019-07-16 17:51:55 -0500

    disable register

--- auth.py
+++ auth.py
@@ -11,28 +11,28 @@ from bson.objectid import ObjectId
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/register', methods=('GET', 'POST'))
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        error = None
+# @bp.route('/register', methods=('GET', 'POST'))
+# def register():
+#     if request.method == 'POST':
+#         username = request.form['username']
+#         password = request.form['password']
+#         error = None
 
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
-        elif find_one('user', condition={"username": username}):
-            error = 'User {} is already registered.'.format(username)
+#         if not username:
+#             error = 'Username is required.'
+#         elif not password:
+#             error = 'Password is required.'
+#         elif find_one('user', condition={"username": username}):
+#             error = 'User {} is already registered.'.format(username)
 
-        if error is None:
-            password_hash = generate_password_hash(password)
-            insert_one('user', document={"username": username, "password_hash": password_hash})
-            return redirect(url_for('auth.login'))
+#         if error is None:
+#             password_hash = generate_password_hash(password)
+#             insert_one('user', document={"username": username, "password_hash": password_hash})
+#             return redirect(url_for('auth.login'))
 
-        flash(error)
+#         flash(error)
 
-    return render_template('auth/register.html')
+#     return render_template('auth/register.html')
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
