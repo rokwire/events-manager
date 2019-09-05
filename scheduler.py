cc7873228a70ccec8a22ca713560c7a372ecfc44
han <hanj2@illinois.edu>
2019-09-05 09:29:29 -0500

    add scheduler debugging logging

--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.events import *
 from .utilities.sourceEvents import start
-
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 def event_exception_listener(event):
     if event.exception:
@@ -22,9 +23,9 @@ def event_completed_listener(event):
     print("Job {} has been completed".format(event.job_id))
 
 def scheduler_add_job(current_app, scheduler, func, start_date, *args, **kwargs):
-    
+
     if not scheduler or not func:
-        print("Error not found scheduler instance or function") 
+        print("Error not found scheduler instance or function")
 
     elif not current_app:
         print("Job here should run under app context")
@@ -39,8 +40,6 @@ def scheduler_add_job(current_app, scheduler, func, start_date, *args, **kwargs)
         except Exception:
             print("Error adding job")
 
-
-
 def create_scheduler(app):
     today = datetime.today()
     startDate = datetime(today.year, today.month, today.day, int(app.config['SCHEDULER_HOUR']), int(app.config['SCHEDULER_MINS']), 00, 00)
@@ -52,7 +51,7 @@ def create_scheduler(app):
     def create_start(app):
         with app.app_context():
             start()
-    
+
     scheduler.add_job(create_start, trigger='interval', args=[app], days=1, start_date=startDate)
     print("Schedule at {}:{}".format(app.config['SCHEDULER_HOUR'], app.config['SCHEDULER_MINS']))
     return scheduler
