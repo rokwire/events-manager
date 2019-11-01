d21a942b6432cec23396c621d8e678e31e3a8d22
Bing Zhang <bing@illinois.edu>
2019-11-01 15:00:34 -0500

    updates

--- utilities/event_time_conversion.py
+++ utilities/event_time_conversion.py
@@ -8,4 +8,4 @@ def utctime(input_datetime):
     datetime_without_tz = datetime.strptime(input_datetime.strftime('%Y-%m-%d %H:%M:%S'), "%Y-%m-%d %H:%M:%S")
     datetime_with_tz = local_tz.localize(datetime_without_tz, is_dst=None)  # No daylight saving time
     datetime_in_utc = datetime_with_tz.astimezone(pytz.utc)
-    return datetime_in_utc.strftime('%Y-%m-%d %H:%M:%S')
+    return datetime_in_utc.strftime('%Y-%m-%dT%H:%M:%S')
