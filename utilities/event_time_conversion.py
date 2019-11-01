c9322ce4a0d4f5675eebd95d22dbfce7bdefd9f1
Bing Zhang <bing@illinois.edu>
2019-11-01 14:58:29 -0500

    updates

--- utilities/event_time_conversion.py
+++ utilities/event_time_conversion.py
@@ -5,8 +5,7 @@ import datetime
 def utctime(input_datetime):
     # FIXME: temporarily use US/Central for webtool events
     local_tz = pytz.timezone("US/Central")
-    datetime_without_tz = datetime.datetime.strptime(input_datetime, "%Y-%m-%d %H:%M:%S")
+    datetime_without_tz = datetime.strptime(input_datetime.strftime('%Y-%m-%d %H:%M:%S'), "%Y-%m-%d %H:%M:%S")
     datetime_with_tz = local_tz.localize(datetime_without_tz, is_dst=None)  # No daylight saving time
     datetime_in_utc = datetime_with_tz.astimezone(pytz.utc)
-
-    return datetime_in_utc.strftime('%Y-%m-%d %H:%M:%S %Z')
+    return datetime_in_utc.strftime('%Y-%m-%d %H:%M:%S')
