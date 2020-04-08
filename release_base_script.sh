3ac0ecab88347a222ae6d56d6957e661782e467f
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2020-04-08 11:30:03 -0500

    Update release script.

--- release_base_script.sh
+++ release_base_script.sh
@@ -9,6 +9,8 @@ if [[ "${GIT_BRANCH}" = "master" ]]; then
     VERSION=${VERSION:-"$(git describe --abbrev=0 --tags)"}
 elif [[ "${GIT_BRANCH}" = "develop" ]]; then
     VERSION="develop"
+elif [[ "${GIT_BRANCH}" = "add-button-to-create-user-event" ]]; then
+    VERSION="demo-apr-10"
 else
     exit 0
 fi
