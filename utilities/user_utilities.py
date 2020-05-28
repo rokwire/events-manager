6038f5862e765fc2f30ceb74de8e7fd6a8336893
Jianyuan Zhan <zhan32@illinois.edu>
2020-05-28 15:50:41 -0500

    Handle network exception on deleting user event from building block (#349)
    * handles the request exception
    
    * updates CHANGLOG.md

--- utilities/user_utilities.py
+++ utilities/user_utilities.py
@@ -109,12 +109,16 @@ def delete_user_event_in_building_block(objectId_list):
     for _id in objectId_list:
         event = find_one(current_app.config['EVENT_COLLECTION'], condition=_id)
         url = current_app.config['EVENT_BUILDING_BLOCK_URL'] + '/' + str(event.get('platformEventId'))
-        result = requests.delete(url, headers=headers)
-        if result.status_code != 202:
-            print("Event {} deletion fails".format(_id))
+        try:
+            result = requests.delete(url, headers=headers)
+            if result.status_code != 202:
+                print("Event {} deletion fails".format(_id))
+                fail_count += 1
+            else:
+                delete_success_list.append(_id)
+        except requests.exceptions.RequestException as err:
+            print("Unexpected network error when deleting user event {}:".format(_id), err)
             fail_count += 1
-        else:
-            delete_success_list.append(_id)
     success_count = len(delete_success_list)
 
     print("failed deleted in building block: " + str(fail_count))
