2f08a4cba13b058feb8281cead006754147b1a46
Jianyuan Zhan <zhan32@illinois.edu>
2020-07-13 14:46:21 -0500

    image endpoint supports S3 (#441)
    * image endpoint supports S3
    
    * typo fix

--- user_events.py
+++ user_events.py
@@ -416,9 +416,30 @@ def searchsub():
 @userbp.route('/event/<id>/image', methods=['GET'])
 @role_required("user")
 def view_image(id):
-    try:
-        image_name = glob(path.join(Config.WEBTOOL_IMAGE_MOUNT_POINT, id + '*'))[0].rsplit('/', 1)[1]
-        directory = path.join(getcwd(), Config.WEBTOOL_IMAGE_MOUNT_POINT.rsplit('/', 1)[1])
-        return send_from_directory(directory, image_name)
-    except IndexError:
-        abort(404)
+    record = find_one(Config.IMAGE_COLLECTION, condition={"eventId": id})
+    if record:
+        success = s3_image_download(id, record.get("_id"))
+        if success:
+            try:
+                print("{}, s3: s3_image_download()".format(record.get('status')))
+                path_to_tmp_image = os.path.join(os.getcwd(), 'temp', id + ".jpg")
+
+                def get_image():
+                    with open(path_to_tmp_image, 'rb') as f:
+                        yield from f
+                    os.remove(path_to_tmp_image)
+
+                response = current_app.response_class(get_image(), mimetype='image/jpg')
+                return response
+            except Exception:
+                traceback.print_exc()
+                print("returning image for event:{} on s3 to user failed".format(id))
+        else:
+            abort(404)
+    else:
+        try:
+            image_name = glob(path.join(Config.WEBTOOL_IMAGE_MOUNT_POINT, id + '*'))[0].rsplit('/', 1)[1]
+            directory = path.join(getcwd(), Config.WEBTOOL_IMAGE_MOUNT_POINT.rsplit('/', 1)[1])
+            return send_from_directory(directory, image_name)
+        except IndexError:
+            abort(404)
