e529e673f4c1c5aec8e5831fb9941158ecbba4e9
Anubhav Ghosh <anubhav3@illinois.edu>
2020-07-14 15:06:47 -0500

    bug fix (#453)
    * bug fix
    
    * Fixes
    
    Co-authored-by: Anubhav Ghosh <anbhvg@Anubhavs-MacBook-Pro.local>

--- utilities/user_utilities.py
+++ utilities/user_utilities.py
@@ -629,9 +629,9 @@ def s3_image_download(eventId, imageId):
         record = find_one(current_app.config['IMAGE_COLLECTION'], condition={"eventId": eventId})
         if record:
             fileobj = '{}/{}/{}.jpg'.format(current_app.config['AWS_IMAGE_FOLDER_PREFIX'], eventId, imageId)
-            # _, tmpfolder = os.path.split(tempfile.mkdtemp())
             tmpfolder = 'temp'
-            os.mkdir(tmpfolder)
+            if not os.path.isdir(tmpfolder):
+                os.mkdir(tmpfolder)
             tmpfile = os.path.join(tmpfolder, eventId + ".jpg")
             with open(tmpfile, 'wb') as f:
                 client.download_fileobj(current_app.config['BUCKET'], fileobj, f)
@@ -651,8 +651,9 @@ def s3_image_download(eventId, imageId):
 def deletefile(tmpfile):
     try:
         if os.path.exists(tmpfile):
-            tmpfolder, _ = os.path.split(tmpfile)
-            shutil.rmtree(tmpfolder)
+            os.remove(tmpfile)
+            # tmpfolder, _ = os.path.split(tmpfile)
+            # shutil.rmtree(tmpfolder)
 
     except Exception as ex:
         pass
