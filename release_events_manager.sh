23e747ce640b6ac40106b93a10a16a367c40111d
Sandeep Puthanveetil Satheesan <sandeeps@illinois.edu>
2020-10-14 12:26:45 -0500

    Update release_events_manager.sh

--- release_events_manager.sh
+++ release_events_manager.sh
@@ -17,6 +17,6 @@
 source release_base_script.sh
 
 ###### EVENTS MANAGER ######
-docker build -t ${PROJECT_NAME}/events-manager:${VERSION} .
+docker build --pull -t ${PROJECT_NAME}/events-manager:${VERSION} .
 docker tag ${PROJECT_NAME}/events-manager:${VERSION} 779619664536.dkr.ecr.us-east-2.amazonaws.com/${PROJECT_NAME}/events-manager:${VERSION}
 docker push 779619664536.dkr.ecr.us-east-2.amazonaws.com/${PROJECT_NAME}/events-manager:${VERSION}
