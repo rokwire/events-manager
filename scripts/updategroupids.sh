1472c7a05ea141b247b083cfaed5129a19ca96c8
Bing Zhang <bing@illinois.edu>
2021-09-15 16:02:25 -0500

    collection name

--- scripts/updategroupids.sh
+++ scripts/updategroupids.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+
+# how to run it: e.g.,
+# ./updategroupids.sh mongodb://localhost:27017/rokwire events-manager
+
   
 MONGO_URI=$1
-DBNAME=$2
+COLLECTIONNAME=$2
 
 
 while IFS=, read -r eventid platformid title createby startdate enddate groupname groupid
 do
     # echo "$eventid, $platformid, $groupid"
-    event=$(mongo $MONGO_URI --quiet --eval "db.$DBNAME.find({_id: ObjectId('"$eventid"')})")
+    event=$(mongo $MONGO_URI --quiet --eval "db.$COLLECTIONNAME.find({_id: ObjectId('"$eventid"')})")
     #echo $event
     if [ ! -z "$event" ]; then
-        update=$(mongo $MONGO_URI --quiet --eval "db.$DBNAME.update({_id: ObjectId('"$eventid"')}, {\$set: {createdByGroupId: '"$groupid"', "isGroupPrivate": false}})")
+        update=$(mongo $MONGO_URI --quiet --eval "db.$COLLECTIONNAME.update({_id: ObjectId('"$eventid"')}, {\$set: {createdByGroupId: '"$groupid"', "isGroupPrivate": false}})")
         echo $eventid $update
     else
         echo "cannot find $eventid"
