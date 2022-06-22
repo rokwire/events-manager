fc66ad8d70e4d836f48bc363e5a94998c581b19d
Jianyuan Zhan <8536258+clav1d@users.noreply.github.com>
2022-06-22 15:21:46 -0500

    Change copyright to 2022

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-#  Copyright 2020 Board of Trustees of the University of Illinois.
+#  Copyright 2022 Board of Trustees of the University of Illinois.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
